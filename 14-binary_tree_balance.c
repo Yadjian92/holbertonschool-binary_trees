7513c53ed0c82f6f07ab85bc2dc6e83896d97cfe
Yadjian92 <10462@holbertonstudents.com>
2025-04-07 20:50:50 +0200

    Add functions for binary tree depth, leaves, balance, and traversal methods

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: The balance factor, or 0 if tree is NULL.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+}
